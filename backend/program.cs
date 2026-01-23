826d544e24d1add2458f3c578f703dc23d5b4797
TishkaaPlay <tikhon42otmyanin@gmail.com>
2026-01-23 19:28:34 +0300

    Added basic backend program

--- backend/program.cs
+++ backend/program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Меня зовут Тотьмянин Т.А.");
+Console.WriteLine(DateTime.Now);
