a814696286c94b9b0c97ae5bfe507d953210e8ca
TishkaaPlay <tikhon42otmyanin@gmail.com>
2026-01-24 17:17:08 +0300

    Добавили: ввод имени, вывод дня недели и логику продолжения

--- backend/program.cs
+++ backend/program.cs
@@ -1,4 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Меня зовут Тотьмянин Т.А.");
-Console.WriteLine(DateTime.Now);
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Меня зовут Тотьмянин Т.А.");
+        Console.WriteLine(DateTime.Now);
+        Console.WriteLine("-----------------------------------");
+
+        Console.WriteLine("Введите своё имя: ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Привет, {name}!");
+        Console.WriteLine(DateTime.Now.DayOfWeek);
+
+        Console.WriteLine("Хотите продолжить? (y/n)");
+        string ans = Console.ReadLine();
+
+        if (ans.ToLower() == "y")
+        {
+            Console.WriteLine("Продолжаем..");
+        } 
+        else
+        {
+            Console.WriteLine("Завершаем..");
+        }
+    }
+}
+
